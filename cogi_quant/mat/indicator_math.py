7037230958e58e02b6b6bedc34c99fc5475f4887
Phillip Korolev <p.korolev1@outlook.com>
2025-06-24 17:53:15 -0400

    Delete cogi_quant/mat/indicator_math.py

--- cogi_quant/mat/indicator_math.py
+++
@@ -1,9 +0,0 @@
-# Module for market indicator formulas
-
-import math
-import mat.stats
-import numpy as np
-from datetime import datetime
-
-def simple_moving_average(quant_data: np.ndarray):
-    pass
